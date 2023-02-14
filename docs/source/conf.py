70b35a3370d9a4cbf1ed6e8700760cd9c99571c1
Mike Zellinger <michael_zellinger@alumni.brown.edu>
2023-02-13 17:19:53 -0800

    update version number in docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = '2022, repliclust'
 author = 'Mike Zellinger'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.1'
+release = '0.0.3'
 
 
 # -- General configuration ---------------------------------------------------
